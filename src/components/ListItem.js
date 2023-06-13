a419ad02c70b6f1ff7cee057b6f69f7de9ce6080
devicepac <bardichedevice@gmail.com>
2023-06-13 15:40:23 +0900

    Feat: 랜더링 최적화 진행중

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ListItem = ({ item, todoData, setTodoData }) => {
+  console.log("Listitem 랜더링", item);
   const btnStyle = {
     color: "#fff",
     float: "right",
