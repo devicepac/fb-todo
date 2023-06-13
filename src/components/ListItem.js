1250431584b59f6973b327cba70d29422374af47
devicepac <bardichedevice@gmail.com>
2023-06-13 15:46:05 +0900

    Feat : 추가수정

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ListItem = ({ item, todoData, setTodoData }) => {
-  console.log("Listitem 랜더링", item);
+  console.log("ListItem 랜더링", item);
   const btnStyle = {
     color: "#fff",
     float: "right",
@@ -17,31 +17,32 @@ const ListItem = ({ item, todoData, setTodoData }) => {
       textDecoration: _completed ? "line-through" : "none",
     };
   };
-  // 이벤트 핸들러
+
+  //이벤트 핸들러
   const handleClick = _id => {
     // 전달된 ID를 검색해서 목록에서 제거
     // 1. 전달된 id로 해당하는 목록 찾아서 제외
     // 2. 새로운 목록으로 갱신해서 화면 리랜더링
-    // 3. 배열의 고치함수 중 filter 를 사용
+    // 3. 배열의 고차함수 중 filter 를 사용
     const newTodoData = todoData.filter(item => item.id !== _id);
     setTodoData(newTodoData);
   };
+
   const handleCompleteChange = _id => {
-    // 중요한 것은 음.. id에 해당하는 것만 수정하면 되지 XXX
+    // 중요한 것은 id의 해당하는 것만 수정하면 된다 xxx
     // state 는 항상 새롭게 만든 내용 즉, 배열로 업데이트 해야 한다.
     // 새로운 배열을 만들어서 set 하자.
     let newTodoData = todoData.map(item => {
       if (item.id === _id) {
-        // completed의 갱신
+        //completed를 갱신
         item.completed = !item.completed;
       }
-
       return item;
     });
     setTodoData(newTodoData);
   };
   return (
-    <div style={getStyle(item.completed)} key={item.id}>
+    <div style={getStyle(item.completed)}>
       {/* defaultChecked : 체크박스에 기본체크 상태 설정 */}
       <input
         type="checkbox"
