ab4857463263958a136edd94764075bc6520c110
devicepac <bardichedevice@gmail.com>
2023-06-15 18:16:00 +0900

    Feat : 회원가입 작성

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import SingUpDiv from "../style/UserCss";
 import { useNavigate } from "react-router-dom";
+// firebase 연동
+import firebase from "../firebase";
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -8,10 +10,22 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
   const [pwConfirm, setPwConfirm] = useState("");
-  const handleSignUp = e => {
+  const handleSignUp = async e => {
     e.preventDefault();
-    // firevase에 회원가입하기
+    try {
+      // firebase 에 회원가입 하기
+      let createUser = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, pw);
+      await createUser.user.updateProfile({
+        name: nickName,
+      });
+      console.log("등록된 정보 : ", createUser.user);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
   return (
     <div className="p-6 mt-5 shadow rounded-md bg-white">
       <h2>Singup</h2>
